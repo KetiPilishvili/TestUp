f6032cbc0dd9da58fc397c1f7d81c498920cf58f
KetiPilishvili <52124129+KetiPilishvili@users.noreply.github.com>
2020-09-01 14:03:03 +0400

    profile-setting page

--- js/main.js
+++ js/main.js
@@ -35,3 +35,23 @@ window.onclick = function(event) {
   	signUpModal.style.display = "none";
   }
 }
+
+$.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data) {
+        var results = data["results"][0];
+        showUserInfo(results);
+    }
+});
+
+// ამ ფუნქციას გადაეცემა არგუმენტად სერვისის მიერ დაბრუნებული ობიექტი
+// ამავე ფუნქციაში ხდება გარკვეული ინფორმაციის ამოღება და html კომპონენტებზე განთავსება
+function showUserInfo(results) {
+    var avatar = results["picture"]["medium"];
+    var name = results["name"]["first"] + " " + results["name"]["last"];
+
+    $("#user-image").attr("src", avatar);
+    $("#name").text(name);
+
+}
---
+++ js/main2.js
@@ -0,0 +1,21 @@
+// for username
+// http მოთხოვნა მიმართავს სერვისს რომელიც რანდომულად აგენერირებს მომხმარებელს 
+$.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data) {
+        var results = data["results"][0];
+        showUserInfo(results);
+    }
+});
+
+// ამ ფუნქციას გადაეცემა არგუმენტად სერვისის მიერ დაბრუნებული ობიექტი
+// ამავე ფუნქციაში ხდება გარკვეული ინფორმაციის ამოღება და html კომპონენტებზე განთავსება
+function showUserInfo(results) {
+    var avatar = results["picture"]["medium"];
+    var name = results["name"]["first"] + " " + results["name"]["last"];
+
+    $("#user-image").attr("src", avatar);
+    $("#name").text(name);
+
+}
